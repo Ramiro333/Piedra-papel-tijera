42a332fda24417967270e02492acbc222bcca781
Ramiro333 <ramirololreyna@gmail.com>
2023-10-20 14:28:28 -0300

    javascript
    pulido detalles, rondas cambiada para usar un for loop, especificado comentarios, ordenado lienas de codigo

--- rock-papper-scissors.js
+++ rock-papper-scissors.js
@@ -5,10 +5,8 @@ function getComputerChoice() {
     const valorSeleccionado = valores[getRandom];
     return valorSeleccionado
 }
-//establecer si gana o pierde
-//print que gano
-let win = 0;
-let lose = 0;
+//establecer si gana o pierde y guardar valores
+//print que gano la ronda
 function game(userChoice, ComputerChoice) { 
     if (userChoice === ComputerChoice) {
         console.log("empate, vuelve a empezar")
@@ -17,10 +15,10 @@ function game(userChoice, ComputerChoice) {
     if (userChoice === "tijera" ) {
         if (ComputerChoice === "papel") {
             console.log("ganaste!");
-            win = win + 1
+            win = win + 1;
         } else {
             console.log("perdiste, la piedra le gana al papel!")
-            lose = lose + 1
+            lose = lose + 1;
         }   
     }
     if (userChoice === "piedra") {
@@ -29,7 +27,7 @@ function game(userChoice, ComputerChoice) {
             win = win + 1;
         }  else {
             console.log ("perdiste, el papel le gana a la piedra!")
-            lose = lose + 1
+            lose = lose + 1;
         }
     }
     if (userChoice === "papel") {
@@ -38,22 +36,23 @@ function game(userChoice, ComputerChoice) {
             win = win + 1;
         } else {
             console.log("perdiste, la tijera le gana al papel!")
-            lose = lose + 1
+            lose = lose + 1;
         }
-    }
 
+    }
 }
-//randomizar eleccion de la maquina
-//hacer que sean 5 rounds
+//usar eleccion de la maquina
 //hacer que el usuario elija
-let playRound = 0;
-do {
-    let ComputerChoice = getComputerChoice()
-    let getChoise = prompt("que eljies?");
-    let userChoice = getChoise.toLowerCase();
-    game(userChoice, ComputerChoice);
-    playRound = playRound + 1
-} while (playRound < 5);
+//hacer que sean 5 rounds
+//setear valores win/lose
+let win = 0;
+let lose = 0;
+for (let playRound = 0; playRound < 5; playRound++) {
+        let ComputerChoice = getComputerChoice()
+        let getChoise = prompt("que eljies?");
+        let userChoice = getChoise.toLowerCase();
+        game(userChoice, ComputerChoice);
+} 
 //decir quien gano
 if (win > lose) {
     console.log("Ganaste la partida !");
@@ -61,7 +60,7 @@ if (win > lose) {
 if (win < lose) {
     console.log("perdiste la partida :(")
 }
-if (win == lose)
+if (win === lose)
     console.log("empate :0")
 console.log("you = " + win);
 console.log("computer = " + lose)
